bf3e268f09f14399a5e8ed728a2ca7813b78b94f
OakAndClay <81421380+OakAndClay@users.noreply.github.com>
2025-05-14 23:34:25 -0400

    Modified Layout
    Moved logo and company name to a header in the layout file.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,20 @@ import { ReactNode } from 'react';
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-gray-100">
+        <header className="bg-white flex items-center justify-left p-6 shadow-2xl">
+          <img 
+            className="w-28 h-28 mr-4 ml-4"
+            src="/PNG Logo B Black.png"
+            alt="Company Logo"
+          />
+          <div>
+            <h1 className="text-center font-cinzel text-6xl font-bold">Oak and Clay</h1>
+            <p className="text-center font-roboto text-xl">Timber Frame Kits and Custom Craftsmanship</p>
+          </div>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-6xl font-cinzel font-bold">
-        Oak and Clay
-      </h1>
-      <p className="text-3xl font-roboto">
-        Timber Frame Kits for DIY Builders
-      </p>
     </div>
   )
 }
